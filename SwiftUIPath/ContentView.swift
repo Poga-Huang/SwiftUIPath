7742f85a6cbf044abc76e80e12bee82feaa49406
poga07 <chris@168money.com.tw>
2025-01-20 09:51:28 +0800

    Initial Commit

---
+++ SwiftUIPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIPath
+//
+//  Created by 黃柏嘉 on 2025/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIPath/SwiftUIPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPathApp.swift
+//  SwiftUIPath
+//
+//  Created by 黃柏嘉 on 2025/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
